1f70e6ab184d7d47d8c0cef254463a45116bc1ff
Adrian <adumosmog@gmail.com>
2022-05-30 03:00:04 +0800

    updated

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-
-
+import os
+import time
 from xml.etree.ElementTree import tostring
-
+import socket.timeout as TimeoutException
 
 def seq_num_format(i):
     message = str(i)
@@ -10,8 +10,28 @@ def seq_num_format(i):
 
     return message
 
-def main():
-    for i in range(1000):
-        print(seq_num_format(i))
 
+def send_payload(payload, uniqueID, transaction_id):
+    clientsocket.settimeout(1)
+    start_time = time.time()
+    m = len(payload)
+    cwnd = int(m/120)
+    index = 0
+    seq_num = 0
+    messages = []
+    last_acked = -1
+    last = "0"
+    curr_time = time.time() - start_time
+    time_frame = m/120
+    while curr_time < 120:
+        if m - index < cwnd:
+            last = "1"
+        end = min(m, index + cwnd)
+        data = payload[index: end]
+        Message = "ID" + uniqueID + "SN" + seq_num_format(seq_num) + transaction_id + "LAST" + last + data
+        messages.append(Message)
+        clientsocket.sendto()
+def main():
+    
+    payload = open(file = filename).read()
 main()
